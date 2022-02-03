65023607c1d3462b24ce2ee635140a46c09dd293
Louis <ldads@hotmail.co.uk>
2022-02-03 16:39:42 +0000

    Rosserial Integration

--- workspace/src/delta_coords/scripts/delta_main_server.py
+++ workspace/src/delta_coords/scripts/delta_main_server.py
@@ -1,14 +1,15 @@
 #! /usr/bin/env python
 import queue
 import roslaunch
-import roslib
+import roslib; roslib.load_manifest("rosserial_arduino")
 import rospy
 from geometry_msgs.msg import Twist
 import actionlib
 from geometry_msgs.msg import Point
 from SendCoords.msg import SendCoordsAction
-#from SendAllCoords.msg import SendAllCoordsAction, SendAllCoordsGoal
+from delta_coords.srv import *
 from std_msgs.msg import String, Bool
+
  
 
 """ Idea of this is the server will recieve a coordinate from the front camera for a red plant.
@@ -17,72 +18,62 @@ The rover will then slowly move in a straight line until the side camera can see
 Both coords will be sent to the delta arm arduino code which will reply with a completed message once it has carried out its service.
 Then follower node can be restarted. A delay must be placed on the delta_coords restart to prevent a loop occuring. """
 
-boolean_val = False
 
 class DeltaMainServer:
    def __init__(self):
       self.server = actionlib.SimpleActionServer('send_coords', Action, self.execute, False)
       self.server.start()
  
-   def bool_callback(boolean):
-      global boolean_val 
-      boolean_val = boolean
+   def bool_callback(self, boolean): 
+      self.boolean_val = boolean
 
    def execute(self, goal):
    # Do lots of awesome groundbreaking robot stuff here
       
-      #Stop Visual Line Follower
-      node = roslaunch.core.Node('follower_visual','Visual Follower')
-      launch = roslaunch.scriptapi.ROSLaunch()
-      process.launch.launch(node)
+      #Stop Visual Line Follower and Rover
+      self.node = roslaunch.core.Node('follower_visual','Visual Follower')
+      self.launch = roslaunch.scriptapi.ROSLaunch()
+      process.launch.launch(self.node)
       process.stop()
 
-      # Rover moves forward until side camera can see red plant
-      msg = Twist()
-      while (Point(0) & Point(1) == 0):
-         msg.linear.x = 1
-         msg.angular.z = 0
-         cmd_pub.publish(msg)
-
-
-      #Message that side camera can see red plant recieved
- 
-      #stop rover
-      msg.linear.x = 0
+      self.msg = Twist()
+      self.msg.linear.x = 0
 
       #Send coords to Delta Arm module
       """Delta coords send string (Point(x), Point_side(y), Point_side(z))
       """
       rospy.sleep(1)
-      point_string = "%d, %d ,%d" % (goal, Point(0), Point(1))
-      coord_pub.publish(point_string)
+      self.point_string = "%d, %d ,%d" % (Point, goal(0), goal(1))
+      self.exit_status = DeltaClient.delta_client(self.point_string)
+      #coord_pub.publish(point_string)
       
       #Recieve reply saying Delta Arm is finished
       """rospy.sleep() until Subscribe receives message confirming Delta done
-      maybe find another method for this actually"""
-      while(boolean_val == False):
-         rospy.sleep(0.5)
-      
-""" Looks like rosserial doesnt support action servers but I'll leave this as a placeholder in case 
-      client = actionlib.SimpleActionClient('send_all_coords', SendAllCoordsAction)
-      client.wait_for_server()
+      maybe find another method for this actually
+      while(self.boolean_val == False):
+         rospy.sleep(0.5)"""
 
-      goal = SendAllCoordsGoal(order=point_string)
-      client.send_goal(goal)
-      client.wait_for_result(rospy.Duration.from_sec(10.0))
- """
       #Restart Visual Node
-      launch.start()
-
-
+      self.launch.start()
       self.server.set_succeeded()
  
- 
+
+class DeltaClient:
+   def delta_client(self,point_string):
+      rospy.wait_for_service('delta_arduino')
+      try:
+         self.actuate = rospy.ServiceProxy('delta_arduino', Test)
+         self.exit_status = self.actuate(point_string)
+         return self.exit_status.output
+      except rospy.ServiceException as e:
+         print("Service call failed: %s"%e)
+
 if __name__ == '__main__':
    rospy.init_node('delta_main_server')
+   server = DeltaMainServer()
    cmd_pub = rospy.Publisher('/cmd_vel',Twist, queue_size=10)
    coord_pub = rospy.Publisher('coords', String, queue_size=10)
-   arduino_sub = rospy.Subscriber('message', Bool, bool_callback, queue_size=5)
-   side_sub = rospy.Subscriber('delta_coord_req_side', Point, queue_size = 10)
-   server = DeltaMainServer()
+   arduino_sub = rospy.Subscriber('message', Bool, server.bool_callback, queue_size=5)
+   under_sub = rospy.Subscriber('delta_coord_req', Point, queue_size = 10)
+   
    rospy.spin()
